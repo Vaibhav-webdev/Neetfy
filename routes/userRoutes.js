41d7fcaae731a69de026b0db89421ce8877b07ff
Vaibhav Shukla <kingshukla221@gmail.com>
2026-03-23 16:31:54 +0530

    Third Commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.get("/chapters/:dynamic", async (req, res) => {
     else if (subject === "physics") Model = Physics;
     else return res.status(400).json({ message: "Invalid subject" });
 
-    const chapters = await Model.find({}, { questions: 0 });
+    const chapters = await Model.find();
     res.json(chapters);
 
   } catch (err) {
